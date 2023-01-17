f49c29bcbf523575208a5a6d1523d46298aafd3a
Mohammed-Mahmmud <mhmedkhaater@gmail.com>
2023-01-17 13:18:43 +0200

    first comment

---
+++ app/Http/Controllers/DropzoneController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Photo;
+use App\Models\User;
+class DropzoneController extends Controller
+{
+    //
+     public function dropzone()
+    {
+        return view('dropzone');
+    }
+     
+    /**
+     * Image Upload Code
+     *
+     * @return void
+     */
+    public function dropzoneStore(Request $request)
+    {
+        $image = $request->file('file');
+    
+        $imageName = time().'.'.$image->extension();
+        $image->move(public_path('images'),$imageName);
+    
+        return response()->json(['success'=>$imageName]);
+    }
+    
+}
